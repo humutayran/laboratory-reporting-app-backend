ebc5689b4a3519157928e8b474f4d6cb6e6aef4e
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-05-18 23:02:20 +0300

    LabReportImpl.saveReport function broken into small functions.

--- src/main/java/com/humut/laboratoryreportingapp/service/concrete/LabReportImpl.java
+++ src/main/java/com/humut/laboratoryreportingapp/service/concrete/LabReportImpl.java
@@ -1,6 +1,8 @@
 package com.humut.laboratoryreportingapp.service.concrete;
 
 import com.humut.laboratoryreportingapp.dto.request.LabReportRequestDto;
+import com.humut.laboratoryreportingapp.dto.request.PatientRequestDto;
+import com.humut.laboratoryreportingapp.dto.response.LabAssistantResponseDto;
 import com.humut.laboratoryreportingapp.dto.response.LabReportResponseDto;
 import com.humut.laboratoryreportingapp.dto.response.PatientResponseDto;
 import com.humut.laboratoryreportingapp.exception.AssistantNotFoundException;
@@ -9,6 +11,7 @@ import com.humut.laboratoryreportingapp.mapper.LabReportMapper;
 import com.humut.laboratoryreportingapp.mapper.PatientMapper;
 import com.humut.laboratoryreportingapp.model.LabAssistant;
 import com.humut.laboratoryreportingapp.model.LabReport;
+import com.humut.laboratoryreportingapp.model.Patient;
 import com.humut.laboratoryreportingapp.repository.LabReportRepository;
 import com.humut.laboratoryreportingapp.service.abstraction.LabAssistantService;
 import com.humut.laboratoryreportingapp.service.abstraction.LabReportService;
@@ -43,21 +46,48 @@ public class LabReportImpl implements LabReportService {
     @Transactional
     public LabReportResponseDto saveReport(LabReportRequestDto labReportRequestDto) {
         LabReport labReport = LabReportMapper.INSTANCE.requestDtoToEntity(labReportRequestDto);
-        if (!labAssistantService.existByHospitalId(labReport.getLabAssistant().getHospitalId())) {
-            throw new AssistantNotFoundException("bulamadım assitant this id :D ");
-        }
-        labReport.setLabAssistant(LabAssistantMapper.INSTANCE.responseDtoToEntity
-                (labAssistantService.findAssistantWithHospitalId(labReport.getLabAssistant().getHospitalId())));
-        Optional<PatientResponseDto> patientWithIdentityNumberOptional =
-                patientService.findPatientWithIdentityNumber(labReport.getPatient().getIdentityNumber());
-        if (patientWithIdentityNumberOptional.isPresent()) {
-            labReport.setPatient(PatientMapper.INSTANCE.responseDtoToEntity(patientWithIdentityNumberOptional.get()));
-        } else {
-            patientService.addPatient(PatientMapper.INSTANCE.entityToRequestDto(labReport.getPatient()));
-        }
-        labReportRepository.save(labReport);
+
+        validateAndSetLabAssistant(labReport);
+        handlePatient(labReport);
+
+        labReport = labReportRepository.save(labReport);
         return LabReportMapper.INSTANCE.entityToResponseDto(labReport);
     }
+
+    private void validateAndSetLabAssistant(LabReport labReport) {
+        String hospitalId = labReport.getLabAssistant().getHospitalId();
+
+        if (!labAssistantService.existByHospitalId(hospitalId)) {
+            throw new AssistantNotFoundException("Assistant not found with hospital ID: " + hospitalId);
+        }
+
+        LabAssistantResponseDto assistantDto = labAssistantService.findAssistantWithHospitalId(hospitalId);
+        LabAssistant labAssistant = LabAssistantMapper.INSTANCE.responseDtoToEntity(assistantDto);
+        labReport.setLabAssistant(labAssistant);
+    }
+
+    private void handlePatient(LabReport labReport) {
+        String identityNumber = labReport.getPatient().getIdentityNumber();
+
+        Optional<PatientResponseDto> patientOptional = patientService.findPatientWithIdentityNumber(identityNumber);
+
+        if (patientOptional.isPresent()) {
+            setExistingPatient(labReport, patientOptional.get());
+        } else {
+            addNewPatient(labReport);
+        }
+    }
+
+    private void setExistingPatient(LabReport labReport, PatientResponseDto patientDto) {
+        Patient patient = PatientMapper.INSTANCE.responseDtoToEntity(patientDto);
+        labReport.setPatient(patient);
+    }
+
+    private void addNewPatient(LabReport labReport) {
+        PatientRequestDto patientRequestDto = PatientMapper.INSTANCE.entityToRequestDto(labReport.getPatient());
+        patientService.addPatient(patientRequestDto);
+    }
+
 
     @Override
     public LabReportResponseDto findReportById(Long id) {
